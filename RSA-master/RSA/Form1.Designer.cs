f24cd0dbd14c009179c297e3d408d899af88caf8
erikdzantiev <81524099+erikdzantiev@users.noreply.github.com>
2024-04-03 18:30:18 +0300

    Add files via upload

---
+++ RSA-master/RSA/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.textBoxQ = new System.Windows.Forms.TextBox();
+            this.textBoxSentence = new System.Windows.Forms.TextBox();
+            this.buttonEncrypted = new System.Windows.Forms.Button();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonUncrypted = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxUncrypted = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите значение P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите значение Q";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите Слово";
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxP.Location = new System.Drawing.Point(223, 14);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.Size = new System.Drawing.Size(100, 22);
+            this.textBoxP.TabIndex = 3;
+            // 
+            // textBoxQ
+            // 
+            this.textBoxQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxQ.Location = new System.Drawing.Point(223, 64);
+            this.textBoxQ.Name = "textBoxQ";
+            this.textBoxQ.Size = new System.Drawing.Size(100, 22);
+            this.textBoxQ.TabIndex = 4;
+            // 
+            // textBoxSentence
+            // 
+            this.textBoxSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSentence.Location = new System.Drawing.Point(223, 106);
+            this.textBoxSentence.Name = "textBoxSentence";
+            this.textBoxSentence.Size = new System.Drawing.Size(368, 22);
+            this.textBoxSentence.TabIndex = 5;
+            // 
+            // buttonEncrypted
+            // 
+            this.buttonEncrypted.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEncrypted.Location = new System.Drawing.Point(24, 145);
+            this.buttonEncrypted.Name = "buttonEncrypted";
+            this.buttonEncrypted.Size = new System.Drawing.Size(142, 31);
+            this.buttonEncrypted.TabIndex = 9;
+            this.buttonEncrypted.Text = "Зашифровать";
+            this.buttonEncrypted.UseVisualStyleBackColor = false;
+            this.buttonEncrypted.Click += new System.EventHandler(this.buttonEncrypted_Click);
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxEncrypted.Location = new System.Drawing.Point(223, 196);
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(368, 22);
+            this.textBoxEncrypted.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(20, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(189, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Зашифр. сообщение:";
+            // 
+            // buttonUncrypted
+            // 
+            this.buttonUncrypted.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonUncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUncrypted.Location = new System.Drawing.Point(24, 236);
+            this.buttonUncrypted.Name = "buttonUncrypted";
+            this.buttonUncrypted.Size = new System.Drawing.Size(142, 32);
+            this.buttonUncrypted.TabIndex = 12;
+            this.buttonUncrypted.Text = "Расшифровать";
+            this.buttonUncrypted.UseVisualStyleBackColor = false;
+            this.buttonUncrypted.Click += new System.EventHandler(this.buttonUncrypted_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(20, 289);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(197, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Расшифр. сообщение:";
+            // 
+            // textBoxUncrypted
+            // 
+            this.textBoxUncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUncrypted.Location = new System.Drawing.Point(223, 287);
+            this.textBoxUncrypted.Name = "textBoxUncrypted";
+            this.textBoxUncrypted.Size = new System.Drawing.Size(368, 22);
+            this.textBoxUncrypted.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(780, 497);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxUncrypted);
+            this.Controls.Add(this.buttonUncrypted);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.buttonEncrypted);
+            this.Controls.Add(this.textBoxSentence);
+            this.Controls.Add(this.textBoxQ);
+            this.Controls.Add(this.textBoxP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.TextBox textBoxQ;
+        private System.Windows.Forms.TextBox textBoxSentence;
+        private System.Windows.Forms.Button buttonEncrypted;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonUncrypted;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxUncrypted;
+    }
+}
+
---
+++ RSA-master/RSA/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public int P;
+        public int Q;
+        public int E;
+        public int D;
+        public int N;
+        public string Sentences;
+        public Dictionary<char, int> Alfavit = new Dictionary<char, int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                Alfavit.Add(alpha[i], (i + 1) % alpha.Length);
+            }
+        }
+
+        private void buttonEncrypted_Click(object sender, EventArgs e)
+        {
+            P = int.Parse(textBoxP.Text);
+            Q = int.Parse(textBoxQ.Text);
+            Sentences = textBoxSentence.Text.ToUpper();
+            StartRSA();
+        }
+
+        public void StartRSA()
+        {
+            N = P * Q;
+            int m = (P - 1) * (Q - 1);
+            E = GetE(m);//////////////
+            D = GetInverseNumberforModule(E, m);///////////////
+            List<int> T = new List<int>();
+            for (int i = 0; i < Sentences.Length; i++)
+            {
+                    T.Add(Alfavit[Sentences[i]]);
+            }
+            List<long> C = new List<long>();
+            for (int i = 0; i < T.Count; i++)
+            {
+                C.Add(PowForMod(T[i], E, N));
+            }
+            ShowEnCrypted(C);
+        }
+
+        public int GetE(int m)///////////
+        {
+            int res = 2;
+            for (int i = 2; i < m; i++)/////////
+            {
+                if (IsPrime(i) && m % i != 0)//////////
+                {
+                    res = i;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        public bool IsPrime(int a)
+        {
+            for (int i = 2; i <= a/2; i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+
+        public int GetInverseNumberforModule(int opr, int mod)
+        {
+            for (int i = 0; i < mod; i++)
+            {
+                if ((opr * i) % mod == 1)
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
+
+        public long PowForMod(int a, int pow, int mod)
+        {
+            long res = 1;
+            for (int i = 0; i < pow; i++)
+            {
+                res *= a;
+                res %= mod;
+            }
+            return res;
+        }
+        public void ShowEnCrypted(List<long> lst)
+        {
+            string s = "";
+            foreach (var item in lst)
+            {
+                s += item.ToString() + ", ";
+            }
+            textBoxEncrypted.Text = s.Substring(0, s.Length-2);
+        }
+        public void ShowUnCrypted(List<long> lst)
+        {
+            string s = "";
+            foreach (var item in lst)
+            {
+                s += Alfavit.FirstOrDefault(x => x.Value == item).Key;
+
+            }
+            textBoxUncrypted.Text = s;
+        }
+
+        private void buttonUncrypted_Click(object sender, EventArgs e)
+        {
+            List<int> C = new List<int>();
+            string[] s = textBoxEncrypted.Text.Split(',');
+            for (int i = 0; i < s.Length; i++)
+            {
+                C.Add(int.Parse(s[i].Trim()));
+            }
+
+            List<long> T = new List<long>();
+            for (int i = 0; i < C.Count; i++)
+            {
+                T.Add(PowForMod(C[i], D, N));
+            }
+            ShowUnCrypted(T);
+        }
+    }
+}
